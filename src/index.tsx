cf1461d33b432ffb2a3db6691d65431c66dd7efd
mommysgoodpuppy <theelectronicfreezer@gmail.com>
2025-01-26 23:31:33 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+
+import React, { useRef, useState, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import * as THREE from 'three'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { XR, createXRStore } from '@react-three/xr'
+
+const store = createXRStore();
+
+export function App() {
+  const [red, setRed] = useState(false)
+  return <>
+    <button onClick={() => store.enterAR()}>Enter AR</button>
+    <button onClick={() => store.enterVR()}>Enter VR</button>
+    <Canvas>
+      <XR store={store}>
+        <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
+          <boxGeometry />
+          <meshBasicMaterial color={red ? 'red' : 'blue'} />
+        </mesh>
+      </XR>
+    </Canvas>
+  </>
+}
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
